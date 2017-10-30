c32672153ce3a01022d4be0ad993ee84f47b972c
David Poxon <david@davidpoxon.com>
2017-10-30 17:07:07 +1000

    Updates

--- main.bundle.js
+++ main.bundle.js
@@ -488,15 +488,11 @@ var _a, _b, _c;
 
 "use strict";
 /* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "a", function() { return environment; });
-// The file contents for the current environment will overwrite these during build.
-// The build system defaults to the dev environment which uses `environment.ts`, but if you do
-// `ng build --env=prod` then `environment.prod.ts` will be used instead.
-// The list of which env maps to which file can be found in `.angular-cli.json`.
-// The file contents for the current environment will overwrite these during build.
 var environment = {
-    production: false,
-    server: 'localhost:28288'
+    production: true,
+    server: 'ginger.azurewebsites.net'
 };
+//ng build --env=prod 
 //# sourceMappingURL=environment.js.map
 
 /***/ }),
